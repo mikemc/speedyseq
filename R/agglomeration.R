f10d02ad2a5e0fbc8de3ddee0058b551a5f2dd6a
Michael McLaren <m.mclaren42@gmail.com>
2019-04-06 14:11:44 -0400

    Add new psmelt and tax_glom functions

---
+++ R/agglomeration.R
@@ -0,0 +1,144 @@
+#' Agglomerate taxa of the same type.
+#'
+#' This method merges species that have the same taxonomy at a certain
+#' taxonomic rank.  Its approach is analogous to \code{\link{tip_glom}}, but
+#' uses categorical data instead of a tree. In principal, other categorical
+#' data known for all taxa could also be used in place of taxonomy, but for the
+#' moment, this must be stored in the \code{taxonomyTable} of the data. Also,
+#' columns/ranks to the right of the rank chosen to use for agglomeration will
+#' be replaced with \code{NA}, because they should be meaningless following
+#' agglomeration.
+#'
+#' NOTE: This is the speedyseq reimplementation of phyloseq's tax_glom
+#' function. It is designed to produce identical results but this has not been
+#' thoroughly tested. Please report any discrepancies!
+#'
+#' @usage tax_glom(physeq, taxrank=rank_names(physeq)[1], NArm=TRUE,
+#' bad_empty=c(NA, "", " ", "\t"))
+#'
+#' @param physeq (Required). \code{\link{phyloseq-class}} or \code{\link{otu_table}}.
+#'
+#' @param taxrank A character string specifying the taxonomic level
+#'  that you want to agglomerate over.
+#'  Should be among the results of \code{rank_names(physeq)}.
+#'  The default value is \code{rank_names(physeq)[1]},
+#'  which may agglomerate too broadly for a given experiment.
+#'  You are strongly encouraged to try different values for this argument.
+#'
+#' @param NArm (Optional). Logical, length equal to one. Default is \code{TRUE}.
+#'  CAUTION. The decision to prune (or not) taxa for which you lack categorical
+#'  data could have a large effect on downstream analysis. You may want to
+#'  re-compute your analysis under both conditions, or at least think carefully
+#'  about what the effect might be and the reasons explaining the absence of 
+#'  information for certain taxa. In the case of taxonomy, it is often a result 
+#'  of imprecision in taxonomic designation based on short phylogenetic sequences
+#'  and a patchy system of nomenclature. If this seems to be an issue for your
+#'  analysis, think about also trying the nomenclature-agnostic \code{\link{tip_glom}}
+#'  method if you have a phylogenetic tree available.
+#'
+#' @param bad_empty (Optional). Character vector. Default: \code{c(NA, "", " ", "\t")}.
+#'  Defines the bad/empty values 
+#'  that should be ignored and/or considered unknown. They will be removed
+#'  from the internal agglomeration vector derived from the argument to \code{tax},
+#'  and therefore agglomeration will not combine taxa according to the presence
+#'  of these values in \code{tax}. Furthermore, the corresponding taxa can be
+#'  optionally pruned from the output if \code{NArm} is set to \code{TRUE}.
+#' 
+#' @return A taxonomically-agglomerated, optionally-pruned, object with class matching
+#' the class of \code{physeq}.
+#'
+#' @seealso
+#' \code{\link{tip_glom}}
+#' 
+#' \code{\link{prune_taxa}}
+#' 
+#' \code{\link{merge_taxa}}
+#' 
+#' @export
+#'
+#' @examples
+#' # data(GlobalPatterns)
+#' # ## print the available taxonomic ranks
+#' # colnames(tax_table(GlobalPatterns))
+#' # ## agglomerate at the Family taxonomic rank
+#' # (x1 <- tax_glom(GlobalPatterns, taxrank="Family") )
+#' # ## How many taxa before/after agglomeration?
+#' # ntaxa(GlobalPatterns); ntaxa(x1)
+#' # ## Look at enterotype dataset...
+#' # data(enterotype)
+#' # ## print the available taxonomic ranks. Shows only 1 rank available, not useful for tax_glom
+#' # colnames(tax_table(enterotype))
+tax_glom <- function(physeq, taxrank=rank_names(physeq)[1],
+					NArm=TRUE, bad_empty=c(NA, "", " ", "\t")){
+    #### This part is identical to phyloseq's tax_glom
+	# Error if tax_table slot is empty
+	if( is.null(access(physeq, "tax_table")) ){
+		stop("The tax_glom() function requires that physeq contain a taxonomyTable")
+	}
+	# Error if bad taxrank
+	if( !taxrank[1] %in% rank_names(physeq) ){
+		stop("Bad taxrank argument. Must be among the values of rank_names(physeq)")		
+	}
+	# Make a vector from the taxonomic data.
+	CN  <- which( rank_names(physeq) %in% taxrank[1] )
+	tax <- as(access(physeq, "tax_table"), "matrix")[, CN]
+	# if NArm is TRUE, remove the empty, white-space, NA values from 
+	if( NArm ){
+		keep_species <- names(tax)[ !(tax %in% bad_empty) ]
+		physeq <- prune_taxa(keep_species, physeq)
+	}
+	# Concatenate data up to the taxrank column, use this for agglomeration
+	tax <- as(access(physeq, "tax_table"), "matrix")[, 1:CN, drop=FALSE]
+	tax <- apply(tax, 1, function(i){paste(i, sep=";_;", collapse=";_;")})
+	# Remove NAs and useless from the vector/factor for looping.
+	# This does not remove the taxa that have an unknown (NA)
+	# taxonomic designation at this particular taxonomic rank.
+	tax <- tax[ !(tax %in% bad_empty) ]
+	# Define the OTU cliques to loop through
+	spCliques <- tapply(names(tax), factor(tax), list)
+    #### **Speedyseq changes start here**
+    ## Make the new OTU table
+    # Starting point is a tibble with rows as taxa, to be able to combine taxa
+    # with the dplyr::summarize_*() functions
+    otu <- otu_table(physeq)
+    if (!taxa_are_rows(physeq)) {
+        otu <- t(otu)
+    }
+    tb <- otu %>%
+        as("matrix") %>%
+        tibble::as_tibble(rownames = "OTU")
+    # We want to name each new taxon (group of merged OTUs) by its "archetype",
+    # the most abundant OTU in the group
+    tb <- tb %>%
+        tibble::add_column(Tax = tax, Sum = taxa_sums(physeq)) %>%
+        dplyr::group_by(Tax)
+    # Name new taxa by the most abundant OTU
+    new_taxa_names <- tb %>% 
+        dplyr::top_n(1, Sum) %>%
+        dplyr::select(Tax, OTU)
+    # Sum abundances and rename taxa
+    tb0 <- tb %>%
+        dplyr::summarize_at(dplyr::vars(sample_names(physeq)), sum) %>%
+        dplyr::left_join(new_taxa_names, by = "Tax") %>%
+        dplyr::select(OTU, dplyr::everything(), -Tax)
+    # Put back into phyloseq form
+    mat <- tb0 %>%
+        dplyr::select(-OTU) %>%
+        as("matrix")
+    rownames(mat) <- tb0$OTU
+    otu0 <- otu_table(mat, taxa_are_rows = TRUE)
+    if (!taxa_are_rows(physeq)) {
+        otu0 <- t(otu0)
+    }
+    ## Make the new phyloseq object
+    # Replacing the original otu_table with the new, smaller table will
+    # automatically prune the taxonomy, tree, and refseq to the smaller set of
+    # archetypal otus
+    otu_table(physeq) <- otu0
+	# "Empty" the taxonomy values to the right of the rank, using
+    # NA_character_.
+    bad_ranks <- seq(CN + 1, length(rank_names(physeq)))
+    tax_table(physeq)[,bad_ranks] <- NA_character_		
+	## Return.
+	return(physeq)
+}
---
+++ R/psmelt.R
@@ -0,0 +1,162 @@
+#' Melt phyloseq data object into large data.frame
+#'
+#' The psmelt function is a specialized melt function for melting phyloseq objects
+#' (instances of the phyloseq class), usually for producing graphics
+#' with \code{\link[ggplot2]{ggplot}2}.
+#' The naming conventions used in downstream phyloseq graphics functions
+#' have reserved the following variable names that should not be used
+#' as the names of \code{\link{sample_variables}}
+#' or taxonomic \code{\link{rank_names}}.
+#' These reserved names are \code{c("Sample", "Abundance", "OTU")}.
+#' Also, you should not have identical names for 
+#' sample variables and taxonomic ranks.
+#' That is, the intersection of the output of the following two functions
+#' \code{\link{sample_variables}}, \code{\link{rank_names}}
+#' should be an empty vector
+#' (e.g. \code{intersect(sample_variables(physeq), rank_names(physeq))}).
+#' All of these potential name collisions are checked-for
+#' and renamed automtically with a warning. 
+#' However, if you (re)name your variables accordingly ahead of time,
+#' it will reduce confusion and eliminate the warnings.
+#'
+#' NOTE: This is the speedyseq reimplementation of phyloseq's psmelt function.
+#' Speedyseq reimplements \code{psmelt} to use functions from the \code{tidyr}
+#' and \code{dplyr} packages.
+#' 
+#' Note that ``melted'' phyloseq data is stored much less efficiently, and so
+#' RAM storage issues could arise with a smaller dataset (smaller number of
+#' samples/OTUs/variables) than one might otherwise expect.  For common sizes
+#' of graphics-ready datasets, however, this should not be a problem.  Because
+#' the number of OTU entries has a large effect on the RAM requirement, methods
+#' to reduce the number of separate OTU entries -- for instance by
+#' agglomerating OTUs based on phylogenetic distance using
+#' \code{\link{tip_glom}} -- can help alleviate RAM usage problems.  This
+#' function is made user-accessible for flexibility, but is also used
+#' extensively by plot functions in phyloseq.
+#'
+#' @usage psmelt(physeq)
+#'
+#' @param physeq (Required). An \code{\link{otu_table-class}} or 
+#'  \code{\link{phyloseq-class}}. Function most useful for phyloseq-class.
+#'
+#' @return A \code{\link{data.frame}}-class table.
+#'
+#' @seealso
+#'  \code{\link{plot_bar}}
+#' 
+#'  \code{\link[reshape2]{melt}}
+#'
+#'  \code{\link{merge}}
+#' 
+#' @importFrom dplyr %>%
+#' 
+#' @export
+#'
+#' @examples
+#' data("GlobalPatterns")
+#' gp.ch = subset_taxa(GlobalPatterns, Phylum == "Chlamydiae")
+#' mdf = psmelt(gp.ch)
+#' nrow(mdf)
+#' ncol(mdf)
+#' colnames(mdf)
+#' head(rownames(mdf))
+#' # Create a ggplot similar to
+#' library("ggplot2")
+#' p = ggplot(mdf, aes(x=SampleType, y=Abundance, fill=Genus))
+#' p = p + geom_bar(color="black", stat="identity", position="stack")
+#' print(p)
+# TODO: test behavior vs. phyloseq's
+psmelt = function(physeq){
+  # Access covariate names from object, if present
+  if(!inherits(physeq, "phyloseq")){
+    rankNames = NULL
+    sampleVars = NULL
+  } else {
+    # Still might be NULL, but attempt access
+    rankNames = rank_names(physeq, FALSE)
+    sampleVars = sample_variables(physeq, FALSE) 
+  }
+  # Define reserved names
+  reservedVarnames = c("Sample", "Abundance", "OTU")  
+  # type-1a conflict: between sample_data 
+  # and reserved psmelt variable names
+  type1aconflict = intersect(reservedVarnames, sampleVars)
+  if(length(type1aconflict) > 0){
+    wh1a = which(sampleVars %in% type1aconflict)
+    new1a = paste0("sample_", sampleVars[wh1a])
+    # First warn about the change
+    warning("The sample variables: \n",
+            paste(sampleVars[wh1a], collapse=", "), 
+            "\n have been renamed to: \n",
+            paste0(new1a, collapse=", "), "\n",
+            "to avoid conflicts with special phyloseq plot attribute names.")
+    # Rename the sample variables.
+    colnames(sample_data(physeq))[wh1a] <- new1a
+  }
+  # type-1b conflict: between tax_table
+  # and reserved psmelt variable names
+  type1bconflict = intersect(reservedVarnames, rankNames)
+  if(length(type1bconflict) > 0){
+    wh1b = which(rankNames %in% type1bconflict)
+    new1b = paste0("taxa_", rankNames[wh1b])
+    # First warn about the change
+    warning("The rank names: \n",
+            paste(rankNames[wh1b], collapse=", "), 
+            "\n have been renamed to: \n",
+            paste0(new1b, collapse=", "), "\n",
+            "to avoid conflicts with special phyloseq plot attribute names.")
+    # Rename the conflicting taxonomic ranks
+    colnames(tax_table(physeq))[wh1b] <- new1b
+  }
+  # type-2 conflict: internal between tax_table and sample_data
+  type2conflict = intersect(sampleVars, rankNames)
+  if(length(type2conflict) > 0){
+    wh2 = which(sampleVars %in% type2conflict)
+    new2 = paste0("sample_", sampleVars[wh2])
+    # First warn about the change
+    warning("The sample variables: \n",
+            paste0(sampleVars[wh2], collapse=", "), 
+            "\n have been renamed to: \n",
+            paste0(new2, collapse=", "), "\n",
+            "to avoid conflicts with taxonomic rank names.")
+    # Rename the sample variables
+    colnames(sample_data(physeq))[wh2] <- new2
+  }
+  # Enforce OTU table orientation. Redundant-looking step
+  # supports "naked" otu_table as `physeq` input.
+  otutab = otu_table(physeq)
+  if(!taxa_are_rows(otutab)){otutab <- t(otutab)}
+  ## Speedyseq specific code starts here
+  # Convert the otu table to a tibble in tall form (one sample-taxon obsevation
+  # per row)
+  tb <- otutab %>% 
+      as("matrix") %>%
+      tibble::as_tibble(rownames = "OTU") %>%
+      tidyr::gather("Sample", "Abundance", -OTU)
+  # Add the sample data if it exists
+  if (!is.null(sampleVars)) {
+      sam <- sample_data(physeq) %>%
+          as("data.frame") %>% 
+          tibble::as_tibble(rownames = "Sample")
+      tb <- tb %>%
+          dplyr::left_join(sam, by = "Sample")
+  }
+  # Add the tax table if it exists
+  if (!is.null(rankNames)) {
+      tax <- tax_table(physeq) %>%
+          as("matrix") %>%
+          tibble::as_tibble(rownames = "OTU")
+      # Convert taxonomy vars to factors for phyloseq compatibiiity
+      if (getOption("stringsAsFactors")) {
+          tax <- tax %>%
+          dplyr::mutate_at(dplyr::vars(-OTU), as.factor)
+      }
+      tb <- tb %>%
+          dplyr::left_join(tax, by = "OTU")
+  }
+  # Arrange by Abundance; TODO: verify this sorting behavior matches phyloseq's
+  tb <- tb %>%
+      dplyr::arrange(desc(Abundance))
+  # Return as a data.frame for phyloseq compatibility
+  tb %>% as.data.frame
+}
