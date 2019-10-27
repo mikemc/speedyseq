498fa9cc9e4d9184d1290c186aff1616771f7907
Michael McLaren <m.mclaren42@gmail.com>
2019-10-27 18:07:38 -0400

    Add tree_glom function
    New function to perform phylogenetic aggregation based on
    `castor::collapse_tree_at_resolution()`

--- R/agglomeration.R
+++ R/agglomeration.R
@@ -1,3 +1,5 @@
+# tax glom --------------------------------------------------------------------
+
 #' Agglomerate taxa of the same type.
 #'
 #' This method merges species that have the same taxonomy at a certain
@@ -139,9 +141,158 @@ tax_glom <- function(physeq, taxrank=rank_names(physeq)[1],
         bad_ranks <- seq(CN + 1, length(rank_names(physeq)))
         tax_table(physeq)[, bad_ranks] <- NA_character_
     }
-	## Return.
     if (needs_flip) {
         physeq <- phyloseq::t(physeq)
     }
-	return(physeq)
+	physeq
 }
+
+# tree glom --------------------------------------------------------------------
+
+#' Agglomerate taxa to a given phylogenetic resolution.
+#'
+#' This method merges taxa that are sufficiently closely related according to
+#' `phy_tree(physeq)`. It uses
+#' \code{\link{castor::collapse_tree_at_resolution}} to determine the groups of
+#' taxa that are to be aggregated and to produce a new tree with each group
+#' collapsed to a single tip. The new taxa are named according to the most
+#' abundant taxon of each group. The reference sequences and taxonomy table
+#' reflect these "archetypal" taxa.
+#'
+#' This method is similar to `phyloseq::tip_glom()` except that it uses the
+#' tree directly, rather than as the source of a distance matrix that is used
+#' for hierarchical clustering. The clustering step of `tip_glom()` is slow and
+#' memory intensive when `ntaxa(physeq)` is large. Since clustering does not
+#' need to be performed, `tree_glom()` is much more efficient.
+#'
+#' @param physeq \code{\link{phyloseq-class}}.
+#' @param resolution Phylogenetic resolution at which to collapse the tree
+#' @param shorten Whether to shorten the terminal branches to the collapsed
+#'   nodes
+#' @param criterion Criterion to determine whether a node is collapsed
+#'
+#' @return A taxonomically-agglomerated, optionally-pruned, object with class matching
+#' the class of \code{physeq}.
+#'
+#' @seealso
+#' \code{\link{castor::collapse_tree_at_resolution}} for more information about
+#' tree-collapsing method and parameters 
+#'
+#' \code{\link{phyloseq::tip_glom}}
+#'
+#' \code{\link{tax_glom}}
+#' 
+#' @export
+#'
+#' @examples
+#' data(GlobalPatterns)
+#' ntaxa(GlobalPatterns)
+#' ps <- tree_glom(GlobalPatterns, 0.03)
+#' ntaxa(ps)
+#'
+tree_glom <- function(physeq, resolution, shorten = TRUE,
+    criterion = "max_tip_depth") {
+
+    # Find the groups of taxa to collapse -------------------------------------
+
+    tree <- phy_tree(physeq)
+    collapsed <- castor::collapse_tree_at_resolution(tree, resolution, 
+        by_edge_count = FALSE, shorten = shorten,
+        rename_collapsed_nodes = TRUE, criterion = criterion)
+
+    # collapsed$collapsed_nodes contains the nodes that were collapsed --- the
+    # tips from each of these nodes tells us the groups of taxa that we need to
+    # merge
+    collapsed_groups <- collapsed$collapsed_nodes %>%
+        purrr::map(~castor::get_subtree_at_node(tree, .)$subtree$tip.label)
+    # Name each group by the member that labels the tip in the collapsed tree
+    tips <- collapsed_groups %>%
+        purrr::map_chr(~ .[. %in% collapsed$tree$tip.label])
+    names(collapsed_groups) <- tips
+    tips <- tibble::enframe(collapsed_groups, "collapsed_tip", "taxon") %>%
+        tidyr::unnest("taxon")
+    # This is for the collapsed nodes only; for the other taxa, we simply have
+    # collapsed_tip = taxon
+    tips <- taxa_names(physeq) %>%
+        setdiff(tips$taxon) %>%
+        rlang::set_names() %>%
+        tibble::enframe("collapsed_tip", "taxon") %>%
+        dplyr::bind_rows(tips)
+    # Now we have the groups, and the name in the collapsed tree
+
+    # Archetypes --------------------------------------------------------------
+
+    # We want to name each new taxon (group of merged OTUs) by its "archetype",
+    # the most abundant OTU in the group
+
+    archetypes <- taxa_sums(physeq) %>%
+        tibble::enframe("taxon", "sum") %>%
+        dplyr::left_join(tips, by = "taxon") %>%
+        dplyr::group_by(collapsed_tip) %>%
+        dplyr::mutate(rank = rank(-sum, ties.method = "first")) %>%
+        dplyr::filter(rank == 1) %>%
+        dplyr::select(collapsed_tip, archetype = taxon)
+
+    tips <- dplyr::left_join(tips, archetypes, by = "collapsed_tip")
+
+    # New tree ----------------------------------------------------------------
+
+    # If shorten == TRUE, then we should keep the new collapsed tree, and
+    # rename the tips to be the archetypes
+    if (shorten == TRUE) {
+        new_tree <- collapsed$tree
+        new_tree$tip.label <- dplyr::left_join(
+                tibble::tibble(collapsed_tip = collapsed$tree$tip.label),
+                archetypes, by = "collapsed_tip") %>%
+            dplyr::pull(archetype)
+    }
+
+    # If shorten == FALSE, then we should stick to the original tree, but prune
+    # to the archetypes
+    if (shorten == FALSE) 
+        new_tree <- prune_taxa(archetypes$archetype, tree)
+
+    # New OTU table -----------------------------------------------------------
+
+    # Starting point is a tibble with rows as taxa, to be able to combine taxa
+    # with the dplyr::summarize_*() functions
+    if (!taxa_are_rows(physeq)) {
+        physeq <- phyloseq::t(physeq)
+        # Note that we need to flip back to samples as rows at the end
+        needs_flip <- TRUE
+    } else {
+        needs_flip <- FALSE
+    }
+    otutb <- otu_table(physeq) %>%
+        as("matrix") %>%
+        tibble::as_tibble(rownames = "taxon") %>%
+        dplyr::left_join(tips, by = "taxon") %>%
+        dplyr::group_by(archetype) %>%
+        dplyr::summarize_at(sample_names(physeq), sum)
+    # Put back into phyloseq form
+    mat <- otutb %>%
+        dplyr::select(-archetype) %>%
+        as("matrix")
+    rownames(mat) <- otutb$archetype
+    new_otu <- otu_table(mat, taxa_are_rows = TRUE)
+
+    # New tax table -----------------------------------------------------------
+
+    # TODO: make a new tax table manually, to figure out which tax ranks are
+    # still valid for the new OTU.
+
+
+
+    # Phyloseq object ---------------------------------------------------------
+
+    # Replacing the original otu_table with the new, smaller table will
+    # automatically prune the taxonomy, tree, and refseq to the smaller set of
+    # archetypal otus
+    phy_tree(physeq) <- new_tree
+    otu_table(physeq) <- new_otu
+    if (needs_flip) {
+        physeq <- phyloseq::t(physeq)
+    }
+	physeq
+}
+
