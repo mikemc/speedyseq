eb5d0300ae1548bd610c49443c7ae71e5bd27f63
Michael McLaren <m.mclaren42@gmail.com>
2021-06-24 15:03:21 -0400

    Exclude additional functions from phyloseq imports
    Speedyseq provides its own `filter_taxa()` and
    `transform_sample_counts()`, which call phloseq's versions explicitly,
    so there is no need to import these.

--- R/import-phyloseq.R
+++ R/import-phyloseq.R
@@ -4,5 +4,5 @@
 #' reimplementing.
 #' 
 #' @name import_phyloseq_functions
-#' @rawNamespace import(phyloseq, except = c(psmelt, plot_bar, plot_tree, plot_heatmap, tax_glom, tip_glom))
+#' @rawNamespace import(phyloseq, except = c(psmelt, plot_bar, plot_tree, plot_heatmap, tax_glom, tip_glom, filter_taxa, transform_sample_counts))
 NULL
